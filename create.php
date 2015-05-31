37ec00f33642ad88f9634d5b5cd2d0bf85fc4eab
abcp4 <navi1921@gmail.com>
2015-05-31 18:02:15 -0300

    Create create.php

---
+++ create.php
@@ -0,0 +1,22 @@
+<h2><?php echo $title ?></h2>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('agenda/create') ?>
+    
+    <label for="login">Title</label>
+    <input type="input" name="login" /><br />
+
+	<label for="pass">Title</label>
+    <input type="input" name="pass" /><br />
+
+    <label for="name">Title</label>
+    <input type="input" name="name" /><br />
+
+	<label for="surname">Title</label>
+    <input type="input" name="surname" /><br />
+
+    <label for="number">Title</label>
+    <input type="input" name="number" /><br />
+
+</form>
